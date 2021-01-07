5179476232e34c79a878ec85f9f2832974621dda
Dimitar Dimitrov <dimitardddm97@gmail.com>
2021-01-07 13:33:57 +0200

    using template if,elseIf and else

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ const app = Vue.createApp({
         return{
             product: {
                 name: 'Socks',
-                description: 'These socks are made pot of 80% Cotton, 18% Polyfills and 2% Elastins',
+                description: 'These socks are made out of 80% Cotton, 18% Polyfills and 2% Elastins',
                 image:'./assets/images/socks_blue.jpg',
-                url: '#'
+                url: '#',
+                inStock: true,
+                quantity: 0
             }
         }
     }
