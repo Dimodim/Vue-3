2abb783f0bb604444b0d856eb3c79fa52fcb3808
Dimitar Dimitrov <65399309+dimospi-MM@users.noreply.github.com>
2021-01-07 12:18:16 +0200

    displaying all the properties of the data object in the app

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const app = Vue.createApp({
         return{
             product: {
                 name: 'Socks',
-                description: 'These socks are made pot of 80% Cotton, 18% Polyfills and 2% Elastins' 
+                description: 'These socks are made pot of 80% Cotton, 18% Polyfills and 2% Elastins',
+                image:'./assets/images/socks_blue.jpg',
+                url: '#'
             }
         }
     }
