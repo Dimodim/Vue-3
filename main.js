9cdbfec0b2a51c8571e44b2adc093c2999ac258e
Dimitar Dimitrov <dimitardddm97@gmail.com>
2021-01-07 19:55:19 +0200

    using computed properties

--- main.js
+++ main.js
@@ -3,23 +3,23 @@ const app = Vue.createApp({
         return {
             product: {
                 name: 'Socks',
-                description: 'These socks are made out of 80% Cotton, 18% Polyfills and 2% Elastins',
-                image:'./assets/images/socks_blue.jpg',
+                description: 'Crisp socks that you can\'t miss on',
                 url: '#',
-                inStock: true,
-                quantity: 0,
-                color: 'blue',
+                brand:'Vue Mastery',
+                selectedVariant: 0,
                 details: ['50% cotton', '30% wool', '20% polyester'],
                 variants: [
                     {
                         id: 2234, 
-                        color: 'dark blue',
-                        image: './assets/images/socks_blue.jpg'
+                        color: 'darkblue',
+                        image: './assets/images/socks_blue.jpg',
+                        quantity: 0
                     },
                     {
                         id: 2235, 
                         color: 'green',
-                        image: './assets/images/socks_green.jpg'
+                        image: './assets/images/socks_green.jpg',
+                        quantity: 14,
                     }
                 ],
                 sizes:[
@@ -29,29 +29,43 @@ const app = Vue.createApp({
                 ]
             },
             cart:[],
-            methods: {
-                addSocksToCart: (color,size)=> {
-                    this.cart.push(
-                        {
-                            product: {
-                                name: this.product.name,
-                                color, 
-                                size,
-                                details:this.product.details,
-                                image: (color === 'green') ?  './assets/images/green.jpg':'./assets/images/socks_blue.jpg',
-                                description: this.product.description,
-                                variant:  (color === 'green') ? this.product.variants[1]:this.product.variants[0]
-                            }
-                        }
-                    );
-                },
-                LogCart: () => {
-                    console.log(this.cart);
-                },
-                changeImage: (image) => {
-                    this.product.image = image;
-                }
-            },
         };
+    },
+    methods: {
+        addSocksToCart() {
+            if(this.inStock){
+                this.cart.push(
+                    {
+                        product: {
+                            name: this.product.name,
+                            color: this.product.variants[this.product.selectedVariant].color, 
+                            details:this.product.details,
+                            image: this.product.variants[this.product.selectedVariant].image,
+                            description: this.product.description,
+                        }
+                    }
+                );
+                --this.product.variants[this.product.selectedVariant].quantity;
+            }
+        },
+        LogCart(){
+            console.log(this.cart);
+        },
+        updateVariant(index){
+            this.product.selectedVariant = index;
+            
+        }
+    },
+    computed: {
+        title(){
+            return this.product.brand + ' ' + this.product.name;
+        },
+        image(){
+            return this.product.variants[this.product.selectedVariant].image;
+        },
+        inStock(){
+            return this.product.variants[this.product.selectedVariant].quantity;
+        }
+
     }
 });
